14f6d828a2be1b173b423019223fad2298e39eac
Orest <orest-2@yandex.ua>
2020-09-12 16:41:58 +0300

    server adjustment

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-import { serve } from "https://deno.land/std@0.66.0/http/server.ts";
-import * as flags from "https://deno.land/std@0.66.0/flags/mod.ts";
+import { serve } from "https://deno.land/std@0.68.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.68.0/http/file_server.ts";
+import * as flags from "https://deno.land/std@0.68.0/flags/mod.ts";
 
 const DEFAULT_PORT = 8081;
 const argPort = flags.parse(Deno.args).port;
@@ -10,9 +11,40 @@ if (isNaN(port)) {
   Deno.exit(1);
 }
 
-const s = serve({ port: port });
-console.log("http://localhost:" + port);
+const server = serve({ port });
+console.log(`server start on :${port}/`);
 
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+async function fileExists(path: string) {
+  try {
+    const stats = await Deno.lstat(path);
+    return stats && stats.isFile;
+  } catch (e) {
+    if (e && e instanceof Deno.errors.NotFound) {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
+const path = (p: string) => `${Deno.cwd()}/client/dist/${p}`;
+
+for await (const req of server) {
+  console.log(req.url);
+
+  if (req.url.includes("/api/")) {
+    req.respond({ body: "Home" });
+    continue;
+  }
+
+  const p = path(req.url);
+  if (await fileExists(p)) {
+    const content = await serveFile(req, p);
+    req.respond(content);
+    continue;
+  } else {
+    const content = await serveFile(req, path("index.html"));
+    req.respond(content);
+    continue;
+  }
 }
